16d1052e6f5e509f65968a782d73dae4370156b6
Adamenko Pavel <padamenko@gmail.com>
2026-02-13 05:48:26 +0300

    working go,js

--- repotest/__init__.py
+++ repotest/__init__.py
@@ -1,4 +1,4 @@
 from repotest.logger import logger
 
-__version__ = "0.5.38"
+__version__ = "0.5.40"
 logger.info("RepoTest initialised %s"%__version__)
---
+++ repotest/manager/__init__.py
@@ -0,0 +1,3 @@
+from repotest.manager.go_liveswebench_task_collector_manager import (  # noqa: F401
+    GoLiveSWEBenchTaskCollectorManager,
+)
